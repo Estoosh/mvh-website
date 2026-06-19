157749e8681e8682e9664614d03557182b1bd795
Estoosh <udi.amrani@gmail.com>
2026-06-19 18:08:34 +0300

    Update [[...index]].js

--- pages/sign-in/[[...index]].js
+++ pages/sign-in/[[...index]].js
@@ -2,7 +2,7 @@ import { SignIn } from '@clerk/nextjs'
 
 export default function SignInPage() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh', background: '#0A0A0A' }}>
+    <div style={{ display: 'flex', justifyContent: 'center', padding: '80px 24px' }}>
       <SignIn />
     </div>
   )
