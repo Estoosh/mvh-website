2f44ec7405a19c85b385042a0503def573a51eca
Estoosh <udi.amrani@gmail.com>
2026-06-19 16:10:57 +0300

    Create admin-data.js

---
+++ pages/api/admin-data.js
@@ -0,0 +1,24 @@
+export default async function handler(req, res) {
+  const token = process.env.AIRTABLE_TOKEN
+  const baseId = process.env.AIRTABLE_BASE_ID
+  const type = req.query.type
+
+  var tableId
+  if (type === 'tours') tableId = 'tbltsGvfPLMAmJ764'
+  else if (type === 'guides') tableId = 'tblsJ5Ok1yPSgtvSj'
+  else if (type === 'signups') tableId = 'Signups'
+  else return res.status(400).json({ error: 'invalid type' })
+
+  try {
+    const response = await fetch(
+      `https://api.airtable.com/v0/${baseId}/${tableId}?pageSize=100`,
+      { headers: { Authorization: `Bearer ${token}` } }
+    )
+    const data = await response.json()
+    const records = (data.records || []).map(function(r) { return Object.assign({ id: r.id }, r.fields) })
+    res.status(200).json({ records })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ records: [] })
+  }
+}
