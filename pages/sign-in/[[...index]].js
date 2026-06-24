9e4feb5fe748a074d596a40cc4ca4c76ad25fbdb
Estoosh <udi.amrani@gmail.com>
2026-06-24 18:55:09 +0300

    Update [[...index]].js

--- pages/sign-in/[[...index]].js
+++ pages/sign-in/[[...index]].js
@@ -1,9 +1,15 @@
 import { SignIn } from '@clerk/nextjs'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
 
 export default function SignInPage() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', padding: '80px 24px' }}>
-      <SignIn />
+    <div style={{ fontFamily: 'Heebo, Arial, sans-serif', background: '#F7F1EA', minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+      <Header />
+      <main style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '64px 24px' }}>
+        <SignIn />
+      </main>
+      <Footer />
     </div>
   )
 }
