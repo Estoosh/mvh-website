81229f8919ac5e46132e20ad5c39b56003e86fa2
Estoosh <udi.amrani@gmail.com>
2026-06-27 22:52:43 +0300

    Update add-tour.js

--- pages/add-tour.js
+++ pages/add-tour.js
@@ -201,7 +201,7 @@ export default function AddTour() {
       setWhatsappNumber(data.guide.WhatsApp_Number || '')
       setForm(function(prev) {
         return Object.assign({}, prev, {
-          guide_context: data.guide.Guide_Bio || ''
+          guide_context: data.guide.Guide_bio || ''
         })
       })
     }
@@ -258,7 +258,7 @@ export default function AddTour() {
           title: form.title,
           city: form.cities,
           guideName: guide?.Guide_Name || '',
-          guideBio: guide?.Guide_Bio || '',
+          guideBio: guide?.Guide_bio || '',
           guideContext: form.guide_context || '',
           historicalPeriods: selectedPeriods,
           field
