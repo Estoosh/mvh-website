491c4acbcae5be1e08f709251f7bfecbc185193b
Estoosh <udi.amrani@gmail.com>
2026-07-01 18:14:13 +0300

    Update telegram-chat-id.js

--- pages/api/telegram-chat-id.js
+++ pages/api/telegram-chat-id.js
@@ -1,12 +1,6 @@
 export default async function handler(req, res) {
   const token = process.env.TELEGRAM_BOT_TOKEN
 
-  if (!token) {
-    return res.status(500).json({
-      error: 'missing_telegram_token'
-    })
-  }
-
   const response = await fetch(
     `https://api.telegram.org/bot${token}/getUpdates`
   )
